083a01eb734031073aa07d0bd95ded0d8eefda80
Keshav Singh <keshavsingh4522@gmail.com>
2023-01-21 18:56:28 +0530

    first package added

---
+++ src/Test_pkg_on_gihub/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Test_pkg_on_gihub;
+
+public static class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Sub(int a, int b)
+    {
+        return a - b;
+    }
+}
